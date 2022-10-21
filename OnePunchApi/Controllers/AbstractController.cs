1c8412df8f871b2b5f20a20061e14a105153ac60
andre <andres.hoyos18@eia.edu.co>
2022-10-21 17:42:23 -0500

    Created AbstractController

---
+++ OnePunchApi/Controllers/AbstractController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnePunchApi.Data.Repository;
+
+namespace OnePunchApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public abstract class AbstractController<T> : ControllerBase
+{
+    // ReSharper disable once MemberCanBePrivate.Global
+    protected readonly IRepository<T> _repo;
+
+    protected AbstractController(IRepository<T> repo)
+    {
+        _repo = repo;
+    }
+
+    [HttpGet]
+    public virtual ActionResult<IEnumerable<T>> GetAll()
+    {
+        return Ok(_repo.GetAll());
+    }
+
+    [HttpGet("{id:int}")]
+    public virtual ActionResult<T?> Get(int id)
+    {
+        var t = _repo.Get(id);
+        if (t is null)
+            return NotFound();
+
+        return Ok(t);
+    }
+
+    // TODO: authorize actions.
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteHero(int id)
+    {
+        var t = _repo.Get(id);
+        if (t is null)
+            return NotFound();
+
+        _repo.Delete(t);
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    public abstract IActionResult CreateHero([FromBody] T t);
+}
