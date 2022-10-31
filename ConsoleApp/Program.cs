d759d998cd5b8d0be88db3a269b7b6d70889b586
Andres_a <andres.hoyos18@eia.edu.co>
2022-10-31 08:41:00 -0500

    .

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// using var client = new HttpClient{BaseAddress = new Uri("https://localhost:7291")};
+//
+// var response = await client.GetAsync("MonsterCell")
--- OnePunchApi/Program.cs
+++ OnePunchApi/Program.cs
@@ -55,6 +55,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
