1212a1359835c94462ec9742782f5cdbe2df4d87
andre <andres.hoyos18@eia.edu.co>
2022-11-02 18:39:39 -0500

    .

---
+++ OnePunchApi/Security/Policies/Handlers/RankHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using OnePunchApi.Security.Policies.Requirements;
+using OnePunchApi.Services;
+
+namespace OnePunchApi.Security.Policies.Handlers;
+
+public class RankHandler : AuthorizationHandler<RankRequirement>
+{
+    private readonly UserManager _userManager;
+
+    public RankHandler(UserManager userManager)
+    {
+        _userManager = userManager;
+    }
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, RankRequirement requirement)
+    {
+        var user = _userManager.GetCurrentUser(context);
+        if (user != null)
+        {
+            if (user.Rank == requirement.Rank)
+                context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
+}
